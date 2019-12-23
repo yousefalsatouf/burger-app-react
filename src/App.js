829fd4e910db4ced96dce5ef8d55c81ce836eb95
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-12-23 13:43:50 +0100

    started implementation the burger app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import Layout from "./components/Layout/Layout";
 
 class App extends Component
 {
     render() {
         return (
-                <div></div>
+            <div>
+                <Layout>
+                    <p>test</p>
+                </Layout>
+            </div>
         );
     }
 }
