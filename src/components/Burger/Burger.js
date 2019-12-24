bc780e8cbf3cbe042ef8f67c9f8102741c37ea60
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-12-24 12:16:53 +0100

    add controls on the gredients sum that the customer can put

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,28 @@ import classes from './Burger.css'
 import Aux from "../../hoc/Auxiliary";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const Burger = () =>
+const Burger = (props) =>
 {
+    // to do map have to transfer object to array ...
+    // again here in case, if i have for ex 2 meat or 2 cheese, i should have them by looping on them using map after transfer it from object to array
+    let objIngredientsToArr = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return <BurgerIngredient key={ingKey + 1} type={ingKey} />
+            })
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    if (objIngredientsToArr.length === 0)
+    {
+        objIngredientsToArr = <p>Please start adding ingredients</p>;
+    }
+
     return(
         <Aux>
             <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top" />
-                <BurgerIngredient type="salad" />
-                <BurgerIngredient type="cheese" />
-                <BurgerIngredient type="meat" />
+                { objIngredientsToArr }
                 <BurgerIngredient type="bread-bottom" />
 
             </div>
