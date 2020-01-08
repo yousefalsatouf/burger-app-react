1b56ffeda4691066bf6496a886b6fc7bf0ab08ad
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-01-08 09:19:09 +0100

    added

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,7 @@ const BuildControls = (props) =>
 {
   return (
       <div className={classes.BuildControls}>
-        <p>Current Price:  <strong>{props.price.toFixed(2)}</strong> </p>
+        <p className={classes.Price}>Current Price:  <strong>{props.price.toFixed(2)}</strong> </p>
         {controls.map(ctrl => {
          return <BuildControl
                   key={ctrl.label}
