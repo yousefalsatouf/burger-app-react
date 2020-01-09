519c2a2113e69c557d9c53da9503cbf4e4972297
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2020-01-09 09:49:48 +0100

    added form validation, but still not added to server

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,15 +2,13 @@ import React from 'react';
 import button from "eslint-plugin-jsx-a11y/src/util/implicitRoles/button";
 import classes from './Button.css'
 
-const Button = (props) => ( <
-    button className = {
-        [classes.Button, classes[props.btnType]].join(' ')
-    }
-    onClick = {
-        props.clicked
-    } > {
-        props.children
-    } < /button>
+const Button = (props) => (
+    <button
+    className={[classes.Button, classes[props.btnType]].join(' ')}
+    onClick={props.clicked}
+    disabled={props.disabled}
+
+    >{props.children}</button>
 );
 
 export default Button;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -21,7 +21,8 @@ class ContactData extends Component
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false,
             },
             street: {
                 elementType: 'input',
@@ -33,7 +34,8 @@ class ContactData extends Component
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false,
             },
             zipCode: {
                 elementType: 'input',
@@ -45,7 +47,8 @@ class ContactData extends Component
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false,
             },
             country: {
                 elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false,
             },
             email: {
                 elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false,
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -79,9 +84,12 @@ class ContactData extends Component
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                validation: {},
+                value: '',
+                valid: true
             },
         },
+        formIsValid: false,
         loading: false,
     };
     orderHandler = ( event ) => {
@@ -113,6 +121,11 @@ class ContactData extends Component
     checkValidity = (value, rules) => {
         let isValid = true;
 
+        if (!rules)
+        {
+            return true;
+        }
+
         if (rules.required)
         {
             isValid = value.trim() !== '' && isValid;
@@ -139,9 +152,16 @@ class ContactData extends Component
 
         updateFormElement.value = event.target.value;
         updateFormElement.valid = this.checkValidity(updateFormElement.value, updateFormElement.validation);
+        updateFormElement.touched = true;
         updateOrderForm[inputIdentifier] = updateFormElement;
 
-        this.setState({orderForm: updateOrderForm})
+        let formIsValid = true;
+        for (let inputIdentifier in updateOrderForm)
+        {
+            formIsValid = updateOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({orderForm: updateOrderForm, formIsValid: formIsValid})
 
     };
 
@@ -167,10 +187,11 @@ class ContactData extends Component
                         value={formElement.config.value}
                         invalid={!formElement.config.valid}
                         shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={(event)=>this.inputChangedHandler(event, formElement.id)}
                     />
                 ))}
-                <Button btnType="Success">Order</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>Order</Button>
             </form>
         );
         if (this.state.loading)
